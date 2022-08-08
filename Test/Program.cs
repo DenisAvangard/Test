0b03a4c946302aa7fd8c860bc733afc1618d8a3b
Den <as_pioneer@mail.ru>
2022-08-08 21:07:19 +0600

    add test program

---
+++ Test/Program.cs
@@ -0,0 +1,27 @@
+﻿string[] array = new string[5] { "1234", "3571", "gb", "don", "red" };
+string[] SortingArr = new string[array.Length];
+
+void ArraySorting(string[] array, string[] SortingArr)
+{
+    int count = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3) 
+        {
+            SortingArr[count] = array[i];
+            count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+ArraySorting(array, SortingArr);
+PrintArray(SortingArr);
